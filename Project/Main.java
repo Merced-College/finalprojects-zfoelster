873dcfe007043c59cd528f6ce62369e85ea5b485
zfoelster <109253459+zfoelster@users.noreply.github.com>
2025-05-02 06:58:24 +0000

    Project Started, also added summary for the assignment.

---
+++ Project/Main.java
@@ -0,0 +1,82 @@
+package Project;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        //Used a HashMap because it is faster than an ArrayList for searching.
+        HashMap<String, Pokemon> pokedex = new HashMap<>();
+
+        //Used an ArrayList because it is easer to add and remove elements.
+        ArrayList<Pokemon> playerPokemon = new ArrayList<>();
+
+        //Get the Pokemon data from the text file pokemon.txt
+        Scanner input = null;
+        try{
+            input = new Scanner(new File("pokemon.txt"));
+        }catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+
+        //I ignore fields[0] because the number is not important information to display.
+        //Since two Pokemon can have the same name if they have a different form, I used the name and form as the key for non-base forms of Pokemon.
+        //For example, "Charizard" and "Charizard:Mega Charizard X" are different keys.
+        while(input.hasNext()){
+            String[] fields = input.nextLine().split(",");
+            Pokemon newPokemon = new Pokemon(fields[1], fields[2], fields[3], fields[4], Integer.parseInt(fields[5]), Integer.parseInt(fields[6]), Integer.parseInt(fields[7]), Integer.parseInt(fields[8]), Integer.parseInt(fields[9]), Integer.parseInt(fields[10]));
+            if(fields[2].isEmpty()){
+                pokedex.put(fields[1], newPokemon);
+            }
+            else{
+            pokedex.put(fields[1] + ":" +  fields[2], newPokemon);
+            }
+        }
+        System.out.println("Welcome to Pokemon Team Builder!");
+        playerPokemon.add(pokedex.get("Charizard"));
+        playerPokemon.add(pokedex.get("Urshifu:Single Strike Style"));
+        printTeam(playerPokemon);
+
+    
+
+    }
+
+
+    //Prints the player's team in a formatted table.
+    //Helped with formatting the printf by GitHub Copilot.
+    public static void printTeam(ArrayList<Pokemon> playerPokemon)
+    {
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("%-5s %-35s %-20s %-8s %-8s %-8s %-8s %-8s %-10s%n", 
+            "Num",
+            "Name",
+            "Type",
+            "HP",
+            "ATK",
+            "DEF",
+            "SP. ATK",
+            "SP. DEF",
+            "SPD");
+            System.out.println("------------------------------------------------------------------------------------------------------------------");
+
+
+        //Loop to print all the Pokemon in the player's team.
+        for (int i = 0; i < playerPokemon.size(); i++) {
+            Pokemon p = playerPokemon.get(i);
+            System.out.printf("%-5d %-35s %-20s %-8d %-8d %-8d %-8d %-8d %-10d%n", 
+                (i + 1), 
+                p.getName(), 
+                p.getType(), 
+                p.getHealth(), 
+                p.getAttack(), 
+                p.getDefense(), 
+                p.getSpAttack(), 
+                p.getSpDefense(),
+                p.getSpeed());
+        }
+    }
+}
---
+++ Project/Pokemon.java
@@ -0,0 +1,80 @@
+package Project;
+
+public class Pokemon {
+private String name;
+private String form;
+private String type1;
+private String type2;
+private int health;
+private int attack;
+private int defense;
+private int spAttack;
+private int spDefense;
+private int speed;
+private String nickname;
+
+    public Pokemon(String name, String form, String type1, String type2, int health, int attack, int defense, int spAttack, int spDefense, int speed) {
+        this.name = name;
+        this.form = form;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.spAttack = spAttack;
+        this.spDefense = spDefense;
+        this.speed = speed;
+    }
+
+//Since Pokemon can have two types, I used an if else statement to check if there is a second type.
+    public String getType(){
+        if(type2.isEmpty()){
+            return type1;
+        }
+        else{
+            return type1 + "/" + type2;
+        }
+    }
+    
+
+    public String getName() {
+        if(form.isEmpty()){
+            return name;
+        }
+        else{
+            return name + " (" + form + ")";
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+    
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpAttack() {
+        return spAttack;
+    }
+
+    public int getSpDefense() {
+        return spDefense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void changeNickname(String nickname){
+        this.nickname = nickname;
+    }
+}
